a75aa739f1e6900f50c6dee798f97c03f348830d
thisislilprincebg-dotcom <this.is.lilprince.bg@gmail.com>
2025-08-24 12:21:04 +0200

    Add files via upload
    Caricamento iniziale del quiz avanzato

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+});
+self.addEventListener('fetch', function(event) {
+  // Puoi aggiungere cache qui
+});
